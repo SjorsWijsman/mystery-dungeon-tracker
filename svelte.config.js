baa5e0d96f1a0861b9e5c18184f6f8f281d47856
Sjors Wijsman <sjorswijsman@gmail.com>
2023-03-12 00:20:16 +0100

    added nojekyll file to build

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/mystery-dungeon-tracker' : ''
 		}
